b48ff315ffaf0809408dba3bc82843d889e24f0b
John Li (Tet) <tet@tet.moe>
2025-06-24 17:22:49 -0700

    fix client not imported properly

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LyricalClient = void 0;
+var Client_1 = require("./src/Client");
+Object.defineProperty(exports, "LyricalClient", { enumerable: true, get: function () { return Client_1.LyricalClient; } });
